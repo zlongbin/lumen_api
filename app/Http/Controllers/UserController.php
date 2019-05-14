83305801c5d4ffab497b825793019f0832cac616
zlongbin <2569050087@qq.com>
2019-05-14 12:53:07 +0000

    注册

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-// header('Access-Control-Allow-Origin:*');
+header('Access-Control-Allow-Origin:*');
 // header('Access-Control-Allow-Methods:*');
 // header('Access-Control-Allow-Headers:*');
 // header("Access-Control-Request-Headers:*");
