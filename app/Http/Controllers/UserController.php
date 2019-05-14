b89c4532c257319105a6663396dcd3953c6bc7af
zlongbin <2569050087@qq.com>
2019-05-14 11:55:32 +0000

    跨域

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ header('Access-Control-Allow-Origin:*');
 header('Access-Control-Allow-Methods:*');
 header('Access-Control-Allow-Headers:*');
 header("Access-Control-Request-Headers:*");
+header('Access-Control-Max-Age:600');
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Crypt;
 use Illuminate\Contracts\Encryption\DecryptException;
@@ -214,6 +215,6 @@ class UserController extends Controller
         // return  time();
     }
     public function ajaxlogin(){
-        
+
     }
 }
