c59258e0af141bbf1d2eb12dcc60dce97bd5fb55
zlongbin <2569050087@qq.com>
2019-05-14 12:37:59 +0000

    header

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,11 +1,11 @@
 <?php
 
 namespace App\Http\Controllers;
-header('Access-Control-Allow-Origin:*');
-header('Access-Control-Allow-Methods:*');
-header('Access-Control-Allow-Headers:*');
-header("Access-Control-Request-Headers:*");
-header('Access-Control-Max-Age:600');
+// header('Access-Control-Allow-Origin:*');
+// header('Access-Control-Allow-Methods:*');
+// header('Access-Control-Allow-Headers:*');
+// header("Access-Control-Request-Headers:*");
+// header('Access-Control-Max-Age:600');
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Crypt;
 use Illuminate\Contracts\Encryption\DecryptException;
